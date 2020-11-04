c1fb90b8906d2fd577dc9462d4cc2228d11d089a
Alejandro Barrios <alejandro.barrios@shopadvizor.com>
2020-11-04 17:00:38 -0300

    Squashed commit of the following:
    commit 68756ec0e733ba6eb16fdabb4bf89b47b8fb61ac
    Author: Alejandro Barrios <alejandro.barrios@shopadvizor.com>
    Date:   Wed Nov 4 16:59:32 2020 -0300
    
        Log requests using morgan

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule.register());
-  await app.listen(3000);
+  app.use(morgan('dev'));
+  await app.listen(5000);
 }
 bootstrap();
