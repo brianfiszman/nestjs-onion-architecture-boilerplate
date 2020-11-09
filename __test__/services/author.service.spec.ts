bcb1c473b95942c78ec0c36dd07168953c0e5420
Alejandro Barrios <alejandro.barrios@shopadvizor.com>
2020-11-09 12:11:12 -0300

    Initial service test

---
+++ __test__/services/author.service.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EntityManager } from '@mikro-orm/core';
+import { AuthorService } from './../../src/domain/services/author';
+import { AuthorRepository } from './../../src/infrastructure/repositories/author';
+
+describe('Author Service', () => {
+  let authorRepository: jest.Mocked<AuthorRepository>;
+  let service: AuthorService;
+
+  beforeEach(async () => {
+    const authorRepositoryMock = {
+      findAll: jest.fn(),
+    };
+    const entityManagerMock = {
+      flush: jest.fn(),
+      getRepository: jest.fn(() => authorRepositoryMock),
+      transactional: jest.fn(async cb => {
+        await cb(entityManagerMock);
+      }),
+    };
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: EntityManager,
+          useValue: entityManagerMock,
+        },
+        {
+          provide: AuthorRepository,
+          useValue: authorRepositoryMock,
+        },
+        AuthorService,
+      ],
+    }).compile();
+
+    authorRepository = module.get(AuthorRepository);
+    service = module.get<AuthorService>(AuthorService);
+  });
+
+  it('Should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
