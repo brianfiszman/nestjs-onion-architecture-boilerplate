118179f7185fe8de3c7d8b2ccccc50262342f385
Alejandro Barrios <alejandro.barrios@shopadvizor.com>
2020-11-09 12:53:09 -0300

    Start app in daemon mode using webpack

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Express + MongoDB + TypeScript example integration
 
-1. Install dependencies via `yarn` or `npm install`
+1. Install dependencies via `yarn`
 2. Run `docker-compose up -d` to start mongodb
 3. Run via `yarn start` or `yarn start:dev` (watch mode)
-4. Example API is running on localhost:3000
+4. Run via `yarn daemon` for reload app in any change (nodemon)
+5. Example API is running on localhost:3000
 
 # Available routes:
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import 'reflect-metadata';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import morgan from 'morgan';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule.register());
   app.use(morgan('dev'));
   await app.listen(5000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
